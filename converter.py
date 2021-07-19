bf9be3741b05a5d09af60357c37c78a9ae4f2819
twchen88 <1030chloechen@gmail.com>
2021-07-19 14:48:10 -0400

    ayayyayayyayayay

--- converter.py
+++ converter.py
@@ -1,7 +1,95 @@
 import tensorflow as tf
+import numpy as np
+import tflite_runtime.interpreter as tflite
+import pathlib
+import matplotlib.pylab as plt
 
-converter = tf.lite.TFLiteConverter.from_saved_model("handwritten_model")
-tflite_model = converter.convert()
+model = keras.models.load_model('handwritten_model', compile=True)
 
-with open("model.tflite", "wb") as f:
-    f.write(tflite_model)
+def representative_data_gen():
+  for input_value in tf.data.Dataset.from_tensor_slices(x_train).batch(1).take(100):
+    yield [input_value]
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_data_gen
+# Ensure that if any ops can't be quantized, the converter throws an error
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# Set the input and output tensors to uint8 (APIs added in r2.3)
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+
+tflite_model_quant = converter.convert()
+
+# Save the quantized model:
+tflite_model_quant_file = pathlib.Path.cwd()/"mnist_model_quant.tflite"
+tflite_model_quant_file.write_bytes(tflite_model_quant)
+
+# Helper function to run inference on a TFLite model
+def run_tflite_model(tflite_file, test_image_indices):
+  global x_test
+
+  # Initialize the interpreter
+  interpreter = tf.lite.Interpreter(model_path=str(tflite_file),
+      experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+  interpreter.allocate_tensors()
+
+  input_details = interpreter.get_input_details()[0]
+  output_details = interpreter.get_output_details()[0]
+
+  predictions = np.zeros((len(test_image_indices),), dtype=int)
+  for i, test_image_index in enumerate(test_image_indices):
+    test_image = x_test[test_image_index]
+    test_label = y_test[test_image_index]
+
+    # Check if the input type is quantized, then rescale input data to uint8
+    if input_details['dtype'] == np.uint8:
+      input_scale, input_zero_point = input_details["quantization"]
+      test_image = test_image / input_scale + input_zero_point
+
+    test_image = np.expand_dims(test_image, axis=0).astype(input_details["dtype"])
+    
+    # reshape test_image
+    test_image = tf.reshape(test_image, [1, 784])
+    
+    interpreter.set_tensor(input_details["index"], test_image)
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details["index"])[0]
+
+    predictions[i] = output.argmax()
+
+  return predictions
+
+
+# Change this to test a different image
+test_image_index = 1
+
+## Helper function to test the models on one image
+def test_model(tflite_file, test_image_index, model_type):
+  global y_test
+
+  predictions = run_tflite_model(tflite_file, [test_image_index])
+
+  tmp = tf.reshape(x_test[test_image_index], [28, 28])
+  plt.imshow(tmp)
+  template = model_type + " Model \n True:{true}, Predicted:{predict}"
+  _ = plt.title(template.format(true= str(y_test[test_image_index]), predict=str(predictions[0])))
+  plt.grid(False)
+
+def getClass(lst):
+    return np.argmax(lst)
+
+
+# Helper function to evaluate a TFLite model on all images
+def evaluate_model(tflite_file, model_type):
+  global x_test
+  global y_test
+
+  test_image_indices = range(x_test.shape[0])
+  predictions = run_tflite_model(tflite_file, test_image_indices)
+  tmp = [getClass(e) for e in y_test]
+
+  accuracy = (np.sum(tmp == predictions) * 100) / len(x_test)
+
+  print('%s model accuracy is %.4f%% (Number of test samples=%d)' % (
+      model_type, accuracy, len(x_test)))
--- main.py
+++ main.py
@@ -1,26 +1,84 @@
-import pycoral
-from pycoral.utils import edgetpu
-from pycoral.utils import dataset
-from pycoral.adapters import common
-from pycoral.adapters import classify
-import matplotlib.pyplot as pyplot
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import tflite_runtime.interpreter as tflite
 
-model_path = "model.tflite"
+# load the data
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+# [60000, 28, 28], [10000, 28, 28], [60000, ], [10000, ]
 
-# check that tpu is available
-print(edgetpu.list_edge_tpus())
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+x_train = x_train.reshape(60000, 28 * 28)/255.0
+x_test = x_test.reshape(10000, 28 * 28)/255.0
 
-# initialize interpreter for tf
-interpreter = edgetpu.make_interpreter(model_path)
-interpreter.allocate_tensors()
+nClass = 10
+y_train = keras.utils.to_categorical(y_train, nClass)
+y_test = keras.utils.to_categorical(y_test, nClass)
 
-# process image
-img = plt.imread("6.jpg")
-img = img[:, :, 0]
-img = cv2.resize(img, (28, 28))
-img = img.reshape(1, 28 * 28)/255
-# img = img.astype("float32")
+# Helper function to run inference on a TFLite model
+def run_tflite_model(tflite_file, test_image_indices):
+  global x_test
 
-common.set_input(interpreter, img)
-interpreter.invoke()
-classes = classify.get_classes(interpreter, top_k=1)
+  # Initialize the interpreter
+  interpreter = tf.lite.Interpreter(model_path=str(tflite_file),
+      experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+  interpreter.allocate_tensors()
+
+  input_details = interpreter.get_input_details()[0]
+  output_details = interpreter.get_output_details()[0]
+
+  predictions = np.zeros((len(test_image_indices),), dtype=int)
+  for i, test_image_index in enumerate(test_image_indices):
+    test_image = x_test[test_image_index]
+    test_label = y_test[test_image_index]
+
+    # Check if the input type is quantized, then rescale input data to uint8
+    if input_details['dtype'] == np.uint8:
+      input_scale, input_zero_point = input_details["quantization"]
+      test_image = test_image / input_scale + input_zero_point
+
+    test_image = np.expand_dims(test_image, axis=0).astype(input_details["dtype"])
+    
+    # reshape test_image
+    test_image = tf.reshape(test_image, [1, 784])
+    
+    interpreter.set_tensor(input_details["index"], test_image)
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details["index"])[0]
+
+    predictions[i] = output.argmax()
+
+  return predictions
+
+
+# Change this to test a different image
+test_image_index = 1
+
+## Helper function to test the models on one image
+def test_model(tflite_file, test_image_index, model_type):
+  global y_test
+
+  predictions = run_tflite_model(tflite_file, [test_image_index])
+
+  tmp = tf.reshape(x_test[test_image_index], [28, 28])
+
+def getClass(lst):
+    return np.argmax(lst)
+
+
+# Helper function to evaluate a TFLite model on all images
+def evaluate_model(tflite_file, model_type):
+  global x_test
+  global y_test
+
+  test_image_indices = range(x_test.shape[0])
+  predictions = run_tflite_model(tflite_file, test_image_indices)
+  tmp = [getClass(e) for e in y_test]
+
+  accuracy = (np.sum(tmp == predictions) * 100) / len(x_test)
+
+  print('%s model accuracy is %.4f%% (Number of test samples=%d)' % (
+      model_type, accuracy, len(x_test)))
+
+evaluate_model("mnist_model_quant.tflite", model_type="Quantized")
